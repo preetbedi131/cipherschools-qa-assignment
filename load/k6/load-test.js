cf7911fe85377efef838e9ac14a3382b0761691b
Preet <150898615+preetbedi131@users.noreply.github.com>
2026-03-05 21:34:06 +0530

    Update load-test.js

--- load/k6/load-test.js
+++ load/k6/load-test.js
@@ -1,1 +1,22 @@
 
+import http from 'k6/http';
+
+export let options = {
+ stages: [
+   { duration: '10s', target: 20 },
+   { duration: '30s', target: 20 },
+   { duration: '10s', target: 0 }
+ ],
+ thresholds: {
+   http_req_duration: ['p(95)<2000'],
+   http_req_failed: ['rate<0.01']
+ }
+};
+
+export default function () {
+
+http.get(
+'https://with-bugs.practicesoftwaretesting.com/api/products/search?q=hammer'
+);
+
+}
